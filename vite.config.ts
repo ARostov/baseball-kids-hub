33725580ef232b9ee8469406bae76a14c35d27a7
arostov <RostovAI@mosreg.ru>
2025-11-25 19:23:26 +0300

    design

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/baseball-kids-hub/',
+    base: './', // Меняем на относительные пути!
     build: {
         outDir: 'dist',
-    },
+        assetsDir: 'assets'
+    }
 })
